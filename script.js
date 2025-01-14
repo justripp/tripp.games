08c9605a72e25997b3946a81a1afa3f9af4dfd90
justripp <105530675+justripp@users.noreply.github.com>
2025-01-14 18:32:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const chessLink = "";
+const pongLink = '';
+const tetrisLink = '';
+const chatLink = 'http://tripp.chat';
+
+const chessBtn = document.getElementById("ChessBtn");
+const chatBtn = document.getElementById("ChatBtn");
+
+
+
+function chessOpen(){
+    window.open("http://172.236.127.104:3037")
+}
+
+function chatOpen(){
+    window.open("https://tripp.chat")
+}
